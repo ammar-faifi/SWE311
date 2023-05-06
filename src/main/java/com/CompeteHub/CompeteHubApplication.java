d0934ab89799ce428338dc954a9dff031de7c13e
ammar-faifi <a@ammarf.com>
2023-05-06 14:19:46 +0300

    try to connect to db

--- src/main/java/com/CompeteHub/CompeteHubApplication.java
+++ src/main/java/com/CompeteHub/CompeteHubApplication.java
@@ -3,10 +3,31 @@ package com.CompeteHub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.ApplicationListener;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+
+import java.util.stream.Stream;
+
 @SpringBootApplication
 public class CompeteHubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CompeteHubApplication.class, args);
 	}
+
+	@Bean
+	ApplicationListener<ApplicationReadyEvent> basicsApplicationListener(TodoRepository repository) {
+		return event -> repository
+				.saveAll(Stream.of("A", "B", "C")
+						.map(name -> new Todo("configuration", "congratulations, you have set up correctly!", true))
+						.toList())
+				.forEach(System.out::println);
+	}
+
+}
+
+interface TodoRepository extends CrudRepository<Todo, Long> {
+
 }
---
+++ src/main/java/com/CompeteHub/Todo.java
@@ -0,0 +1,20 @@
+import org.springframework.data.annotation.Id;
+
+public class Todo {
+
+  public Todo() {}
+
+  public Todo(String description, String details, boolean done) {
+    this.description = description;
+    this.details = details;
+    this.done = done;
+  }
+
+  @Id private Long id;
+
+  private String description;
+
+  private String details;
+
+  private boolean done;
+}

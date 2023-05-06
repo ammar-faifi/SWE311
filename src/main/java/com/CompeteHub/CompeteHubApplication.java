03ca6b2a400ff0a7a0be351add57e61ceeb49583
ammar-faifi <a@ammarf.com>
2023-05-06 09:57:51 +0300

    add simple `/` response

--- src/main/java/com/CompeteHub/CompeteHubApplication.java
+++ src/main/java/com/CompeteHub/CompeteHubApplication.java
@@ -2,12 +2,18 @@ package com.CompeteHub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class CompeteHubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CompeteHubApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	String test() {
+		return "TEST";
 	}
 
 }

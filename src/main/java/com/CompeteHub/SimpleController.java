c69f4e161784683543eacb1f360db8f4eff938d3
ammar-faifi <a@ammarf.com>
2023-05-11 19:36:06 +0300

    fix template name

--- src/main/java/com/CompeteHub/SimpleController.java
+++ src/main/java/com/CompeteHub/SimpleController.java
@@ -2,7 +2,6 @@ package com.CompeteHub;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,23 +9,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleController {
-	private static final String template = "You gave %s";
-	private final AtomicLong counter = new AtomicLong();
+  private static final String template = "You gave %s";
+  private final AtomicLong counter = new AtomicLong();
 
-	@GetMapping("/echo")
-	public EchoType echo(@RequestParam(value = "msg", defaultValue = "World") String value) {
-		return new EchoType(counter.incrementAndGet(), String.format(template, value));
-	}
+  @GetMapping("/echo")
+  public EchoType echo(@RequestParam(value = "msg", defaultValue = "World") String value) {
+    return new EchoType(counter.incrementAndGet(),
+        String.format(template, value));
+  }
 
-	@GetMapping("/")
-	public String test() {
-		return "TEST";
-	}
+  @GetMapping("/")
+  public String test() {
+    return "index";
+  }
 
-	@GetMapping("/randRR")
-	@ResponseBody
-	public List<List<String>> generateRoundRobin(@RequestParam(value = "num", defaultValue = "4") int num) {
-		System.out.println(num);
-		return RoundRobin.generate(num);
-	}
+  @GetMapping("/randRR")
+  @ResponseBody
+  public List<List<String>> generateRoundRobin(@RequestParam(value = "num", defaultValue = "4") int num) {
+    System.out.println(num);
+    return RoundRobin.generate(num);
+  }
 }
